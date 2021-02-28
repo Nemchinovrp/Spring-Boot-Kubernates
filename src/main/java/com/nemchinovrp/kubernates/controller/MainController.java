a3cdf38c88a029c765d86f1371b961afa950437d
nemchinovrp <nemchinovrp@yandex.ru>
2021-02-28 18:33:43 +0800

    added databases

--- src/main/java/com/nemchinovrp/kubernates/controller/MainController.java
+++ src/main/java/com/nemchinovrp/kubernates/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.nemchinovrp.kubernates.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping("/api")
 public class MainController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("test")
     public String test() {
         return "test - test";
+    }
+
+    @GetMapping("connect")
+    public String connection() throws SQLException {
+        return jdbcTemplate.toString() + " - "
+                + jdbcTemplate.getDataSource().getConnection().getCatalog();
     }
 }
